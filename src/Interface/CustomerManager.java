0bbc3f79cf214ff53f14c30e9c52c9e06fae222a
Mehmet Enes Yildirim <mhmtensyldrm71@gmail.com>
2022-10-17 23:46:09 +0300

    customer class modified

--- src/Interface/CustomerManager.java
+++ src/Interface/CustomerManager.java
@@ -1,5 +1,17 @@
 package Interface;
 
 public class CustomerManager {
+    ICustomerDal customerDal;
+
+    public CustomerManager(ICustomerDal customerDal){
+        this.customerDal = customerDal;
+    }
+
+    public void Add(){
+        //Islemler
+        //Islemler
+        //Islemler
+        customerDal.Add();
+    }
     
 }
