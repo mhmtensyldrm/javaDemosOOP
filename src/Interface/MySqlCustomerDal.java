c728ee31d3125a3ee446f6d19109f6a561f3c6df
Mehmet Enes Yildirim <mhmtensyldrm71@gmail.com>
2022-10-17 23:40:56 +0300

    mysql class modified

--- src/Interface/MySqlCustomerDal.java
+++ src/Interface/MySqlCustomerDal.java
@@ -1,7 +1,15 @@
 package Interface;
 
-public class MySqlCustomerDal{
+public class MySqlCustomerDal implements ICustomerDal{
 
     
+    public void Add() {
+        //Database kodları ve islemleri
+        //Database kodları ve islemleri
+        //Database kodları ve islemleri
+        System.out.println("Veriler MySql'e eklendi.");
+    }
+
+
 
 }
