dda691c19809f20c71bb00bdd95cfee960dcfbc3
Mehmet Enes Yildirim <mhmtensyldrm71@gmail.com>
2022-10-17 23:41:40 +0300

    oracle class modified

--- src/Interface/OracleCustomerDal.java
+++ src/Interface/OracleCustomerDal.java
@@ -1,5 +1,13 @@
 package Interface;
 
-public class OracleCustomerDal {
+public class OracleCustomerDal implements ICustomerDal{
     
+    public void Add() {
+        //Database kodları ve islemleri
+        //Database kodları ve islemleri
+        //Database kodları ve islemleri
+        System.out.println("Veriler MySql'e eklendi.");
+    }
+
 }
+
