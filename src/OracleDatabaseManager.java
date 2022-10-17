8ac227eb95f7916d25ecbd60faec8e44927daa46
Mehmet Enes Yildirim <mhmtensyldrm71@gmail.com>
2022-10-17 17:10:28 +0300

    class modifed

--- src/OracleDatabaseManager.java
+++ src/OracleDatabaseManager.java
@@ -1,3 +1,10 @@
-public class OracleDatabaseManager{
-    
+public class OracleDatabaseManager extends BaseDatabaseManager{
+
+    public void getData(){
+        // Veritabanından verileri çekmek için gereken kodlar
+        // Veritabanından verileri çekmek için gereken kodlar
+        // Veritabanından verileri çekmek için gereken kodlar
+        System.out.println("Oracle verisi listelendi.");
+    }
+
 }
--- src/SqlServerDatabaseManager.java
+++ src/SqlServerDatabaseManager.java
@@ -1,3 +1,10 @@
-public class SqlServerDatabaseManager{
-    
+public class SqlServerDatabaseManager extends BaseDatabaseManager{
+
+    public void getData(){
+        // Veritabanından verileri çekmek için gereken kodlar
+        // Veritabanından verileri çekmek için gereken kodlar
+        // Veritabanından verileri çekmek için gereken kodlar
+        System.out.println("SqlServer verisi listelendi.");
+
+    }
 }
