affb1847589f6f853d062a8065ce168d9d942b4a
Mehmet Enes Yildirim <mhmtensyldrm71@gmail.com>
2022-10-17 17:13:07 +0300

    class modified

--- src/MySqlDatabaseManager.java
+++ src/MySqlDatabaseManager.java
@@ -1,3 +1,11 @@
-public class MySqlDatabaseManager {
+public class MySqlDatabaseManager extends BaseDatabaseManager{
+
+    
+    public void getData() {
+        // Veritabanından verileri çekmek için gereken kodlar
+        // Veritabanından verileri çekmek için gereken kodlar
+        // Veritabanından verileri çekmek için gereken kodlar
+        System.out.println("Mysql verisi listelendi.");
+    }
     
 }
