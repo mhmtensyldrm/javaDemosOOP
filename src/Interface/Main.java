2afe3fe243a669645a52c01956231c1c0a714689
Mehmet Enes Yildirim <mhmtensyldrm71@gmail.com>
2022-10-17 23:48:17 +0300

    main class modified

--- src/Interface/Main.java
+++ src/Interface/Main.java
@@ -3,7 +3,8 @@ package Interface;
 public class Main {
     public static void main(String[] args){
 
-        
+        CustomerManager customerManager = new CustomerManager(new OracleCustomerDal());
+        customerManager.Add();
 
     }
 }
