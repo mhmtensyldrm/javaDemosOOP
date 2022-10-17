e06e68c6ba301ade5a199c43910ad95b23491ed1
Mehmet Enes Yildirim <mhmtensyldrm71@gmail.com>
2022-10-17 23:38:09 +0300

    Interface folder and classes added

---
+++ src/Interface/ICustomerDal.java
@@ -0,0 +1,5 @@
+package Interface;
+
+public interface ICustomerDal {
+    public void Add();
+}
---
+++ src/Interface/Main.java
@@ -0,0 +1,9 @@
+package Interface;
+
+public class Main {
+    public static void main(String[] args){
+
+        
+
+    }
+}
