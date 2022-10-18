8f92a224bd16b1869cef19ddafd79763ef2e6166
Mehmet Enes Yildirim <mhmtensyldrm71@gmail.com>
2022-10-18 19:14:07 +0300

    customerdal class added

---
+++ src/Generics/CustomerDal.java
@@ -0,0 +1,23 @@
+package Generics;
+
+public class CustomerDal implements IEntityRepository{
+
+    @Override
+    public void Add(Object entity) {
+        
+        
+    }
+
+    @Override
+    public void Update(Object entity) {
+        
+        
+    }
+
+    @Override
+    public void Delete(Object entity) {
+        
+        
+    }
+    
+}
