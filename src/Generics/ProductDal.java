d299d9f33a60c8d344f0e3907ab923cd4e60218c
Mehmet Enes Yildirim <mhmtensyldrm71@gmail.com>
2022-10-18 19:21:25 +0300

    productdal class added

---
+++ src/Generics/ProductDal.java
@@ -0,0 +1,23 @@
+package Generics;
+
+public class ProductDal implements IEntityRepository<Product>{
+
+    @Override
+    public void Add(Product entity) {
+        
+        
+    }
+
+    @Override
+    public void Update(Product entity) {
+        
+        
+    }
+
+    @Override
+    public void Delete(Product entity) {
+        
+        
+    }
+    
+}
